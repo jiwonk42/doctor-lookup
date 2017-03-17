6cf09318c2b8c5fda747516298263f9a0f4ecb83
Jiwon Kang <jiwonk42@gmail.com>
2017-03-17 10:06:15 -0700

    add a README.md file including all details

---
+++ README.md
@@ -0,0 +1,37 @@
+# _Doctor Lookup_
+
+#### _3.17.2017_
+
+#### By _**Jiwon Kang**_
+
+## Description
+
+_This application will allow the user to look up doctors who specialized in any medical issue entered by the user_
+
+## Setup Instructions
+* Clone this repository
+```
+$ npm install
+$ bower install
+```
+* Open index.html with a web browser
+```
+$ gulp build
+$ gulp serve
+```
+
+## Support and contact details
+
+_Contact: Jiwon Kang at jiwonk42@gmail.com_
+
+## Technologies Used
+
+_HTML, CSS, Bootstrap, JavaScript, API_
+
+## Link:[Click here](https://jiwonk42.github.io/doctor-lookup/)
+
+### License
+
+*MIT*
+
+Copyright (c) 2017 **_Jiwon Kang_**
--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,13 +1,17 @@
 var apiKey = require('./../.env').apiKey;
+var Doctor = require('./../js/doctor.js').doctorModule;
 
 $(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    var city = $('#location').val();
-    $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+  $('#doctor-form').submit(function() {
+    event.preventDefault();
+
+    var userInput = $('#medical-issue').val();
+    $('#userInput').val("");
+
+    $.get('http://api.opendoctormap.org/data/2.5/doctor?q=' + city + '&appid=' + apiKey).then(function(response) {
+      $('.showDoctor').text("The humidity in " + city + " is " + response.main.humidity + "%");
     }).fail(function(error) {
-      $('.showWeather').text(error.responseJSON.message);
+      $('.showDoctor').text(error.responseJSON.message);
     });
   });
 });
