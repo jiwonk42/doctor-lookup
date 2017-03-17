313be7e614285c2e01b62d14516cf0b479bc6e94
Jiwon Kang <jiwonk42@gmail.com>
2017-03-17 14:08:09 -0700

    complete building doctor.js and doctor-interface.js just the basics

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,13 +1,17 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
+
 var displayDoctors = function(firstName, lastName, title, specialty, description) {
-  $('ul#doctor-list').append("<li>" + firstName + " " + lastName + " " + title + " (Speciality: " + specialty + "; " + description + ")</li>");
+  $('#doctor-list').append("<li>" + firstName + " " + lastName + " " + title + " (Speciality: " + specialty + "; " + description + ")</li>");
 }
 
 $(document).ready(function() {
-  var currentDoctorObject = new Doctor();
-  $('#doctor-form').click(function() {
 
+  var currentDoctorObject = new Doctor();
+
+  $('#doctor-form').click(function() {
     event.preventDefault();
+    $("ul").empty();
+
     var userInput = $('input#medical-issue').val().toLowerCase();
     currentDoctorObject.getDoctors(userInput, displayDoctors);
     $('#userInput').val("");
--- js/doctor.js
+++ js/doctor.js
@@ -5,30 +5,30 @@ function Doctor(){
 
 Doctor.prototype.getDoctors = function(medicalIssue, displayDoctors) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&skip=0&limit=10&user_key=' + apiKey)
-   .then(function(result) {
+  .then(function(result) {
 
-     var firstName = "";
-     var lastName = "";
-     var title = "";
-     var specialty = "";
-     var description = "";
+    var firstName = "";
+    var lastName = "";
+    var title = "";
+    var specialty = "";
+    var description = "";
 
-     for (var i = 0; i < result.data.length; i++) {
-       firstName = result.data[i].profile["first_name"];
-       lastName = result.data[i].profile["last_name"];
+    for (var i = 0; i < result.data.length; i++) {
+      firstName = result.data[i].profile["first_name"];
+      lastName = result.data[i].profile["last_name"];
 
-       if (result.data[i].profile["title"]) {
-         title = result.data[i].profile["title"];
-       }
+      if (result.data[i].profile["title"]) {
+        title = result.data[i].profile["title"];
+      }
 
-       specialty = result.data[i].specialties[0].name;
-       description = result.data[i].specialties[0].description;
-       displayDoctors(firstName, lastName, title, specialty, description);
-     }
-    })
-   .fail(function(error){
-      $('.error-message').text(error.responseJSON.message);
-    });
+      specialty = result.data[i].specialties[0].name;
+      description = result.data[i].specialties[0].description;
+      displayDoctors(firstName, lastName, title, specialty, description);
+    }
+  })
+  .fail(function(error){
+    $('.error-message').text(error.responseJSON.message);
+  });
 };
 
 exports.doctorModule = Doctor;
