5b70caf8eb0e9484a1995c047a970616475826a1
Jiwon Kang <jiwonk42@gmail.com>
2017-03-17 14:31:15 -0700

    add h3 Doctors found when results are found

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,6 +1,7 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayDoctors = function(firstName, lastName, title, specialty, description) {
+  $('h2').text("Doctors found:");
   $('#doctor-list').append("<li>" + firstName + " " + lastName + " " + title + " (Speciality: " + specialty + "; " + description + ")</li>");
 }
 
@@ -10,6 +11,7 @@ $(document).ready(function() {
 
   $('#doctor-form').click(function() {
     event.preventDefault();
+    $("h2").empty();
     $("ul").empty();
 
     var userInput = $('input#medical-issue').val().toLowerCase();
